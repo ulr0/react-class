6c557a57f4ede8ef734ae47c5075c414e5969d10
ulr0 <ulr0xb@gmail.com>
2022-01-11 17:05:38 +0900

    main page - navbar - post list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, titleChange] = useState('코트 추천');
+  let posts = '강남 맛집'
+  function a(){
+    return 100
+  }
+  let b = { color : 'pink', fontSize : '30px' }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={ b }> 개발 Blog </div>
+      </div>
+      <div className="list">
+        <h4> { posts } </h4>
+        <p>1월 11일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { a() } </h4>
+        <p>1월 9일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { title } </h4>
+        <p>1월 5일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
