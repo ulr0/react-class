1a0662c0b02a750452febb2ed37905f44df0e3ea
ulr0 <ulr0xb@gmail.com>
2022-01-11 23:15:13 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -15,13 +15,15 @@ function App() {
     newArray[0] = '여자 코트 추천'; // copy한 데이터에 수정사항 반영
     setTitle( newArray ); // 변경함수()에 집어넣기
   }
+  
+  let [modal, setModal] = useState(false); // modal 상태 저장하는 state
 
   return (
     <div className="App">
       <div className="black-nav">
         <div style={ a }> 개발 Blog </div>
       </div>
-      <button onClick={ changeTitle }>버튼</button>
+      {/* <button onClick={ changeTitle }>버튼</button> */}
       <div className="list">
         <h4> { title[0] } <span onClick={ ()=>{ setLike(like + 1) } }>❤️</span> { like } </h4>
         <p>1월 11일 발행</p>
@@ -33,19 +35,18 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h4> { title[1] } </h4>
+        <h4 onClick={ ()=>{ setModal(true) } }> { title[1] } </h4>
         <p>1월 5일 발행</p>
         <hr/>
       </div>
-{/* 
-      <div className="modal">
-        <h2>제목</h2>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div> */}
+    
+        <button onClick={ ()=>{ setModal(!modal) } }>버튼</button>
+          {
+            modal === true
+            ? <Modal></Modal>
+            : null // 텅 빈 HTML
+          }
 
-      <Modal />
-      
     </div>
   );
 }
