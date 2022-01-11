24ee705e707edf620943e37bfd62958d67100246
ulr0 <ulr0xb@gmail.com>
2022-01-11 17:11:59 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import './App.css';
 function App() {
 
   let [title, titleChange] = useState('코트 추천');
-  let posts = '강남 맛집'
-  function a(){
-    return 100
-  }
-  let b = { color : 'pink', fontSize : '30px' }
+  let [title2, titleChange2] = useState('강남 맛집');
+  let [title3, titleChange3] = useState(100);
+  let a = { color : 'pink', fontSize : '30px' }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ b }> 개발 Blog </div>
+        <div style={ a }> 개발 Blog </div>
       </div>
       <div className="list">
-        <h4> { posts } </h4>
+        <h4> { title } </h4>
         <p>1월 11일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h4> { a() } </h4>
+        <h4> { title2 } </h4>
         <p>1월 9일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h4> { title } </h4>
+        <h4> { title3 } </h4>
         <p>1월 5일 발행</p>
         <hr/>
       </div>
